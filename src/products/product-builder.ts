02a8ca5c909b9173f9fa5f525ac44a4a34a4f7fe
Ali Ardestani <ali.sade@gmail.com>
2021-07-28 20:37:34 -0700

    remove template dir, use new Level 2 constructs

--- src/products/product-builder.ts
+++ src/products/product-builder.ts
@@ -1,41 +1,18 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ProductTemplate } from './product.model';
-import { writeFileSync, existsSync, mkdirSync } from 'fs';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { ProductTemplate } from "./product.model";
 
-export const getProductTemplate = (product: ProductTemplate) => {
+export const synthProduct = (product: ProductTemplate) => {
   const app = new cdk.App({
     runtimeInfo: false,
     stackTraces: false,
-    treeMetadata: false
+    treeMetadata: false,
   });
 
-  const defEnv = {
-    env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: 'eu-west-1'
-    }
-  };
+  new product.stack(app, product.name);
 
-  new product.stack(app, product.name, defEnv);
+  app.synth();
 
-  const synth = app.synth();
-
-  const template = synth.getStackArtifact(product.name).template;
-
-  const templateDir = './templates';
-
-  if (!existsSync(templateDir)) {
-    mkdirSync(templateDir);
-  }
-  
-  const templatePath = `${templateDir}/${product.name}.template.json`;
-
-  writeFileSync(templatePath, JSON.stringify(template, null, 2));
-
-  return {
-    template,
-    templatePath
-  };
-}
+  return product.name;
+};
--- src/service-catalog.ts
+++ src/service-catalog.ts
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { S3BucketProduct } from './products/s3-bucket';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { S3BucketProduct } from "./products/s3-bucket";
+import { existsSync } from "fs";
 
 export class ServiceCatalogStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new S3BucketProduct(this, 'S3BucketProduct');
+    // we depend on the synth phase to have been performed
+    if (!existsSync("cdk.out")) {
+      return;
+    }
+    new S3BucketProduct(this, "S3BucketProduct");
   }
 }
 
 const app = new cdk.App();
-new ServiceCatalogStack(app, 'ServiceCatalogStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: 'eu-west-1'
-  }
+
+if (!process.env.AWS_REGION)
+  throw Error("must set environment variable: AWS_REGION");
+
+const { AWS_REGION, CDK_DEFAULT_ACCOUNT } = process.env;
+const env = { account: CDK_DEFAULT_ACCOUNT, region: AWS_REGION };
+
+new ServiceCatalogStack(app, "ServiceCatalogStack", {
+  env,
 });
